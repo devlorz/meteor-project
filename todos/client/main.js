81d31c09bf7c627ac46fda563bfdb83edaa64b7e
Nutti Saelor <leelorz6@gmail.com>
2017-01-11 19:02:50 +0700

    add add check delete function

--- todos/client/main.js
+++ todos/client/main.js
@@ -7,7 +7,33 @@ Todos = new Mongo.Collection('todos');
 
 Template.main.helpers({
   todos: function() {
-    return Todos.find();
+    return Todos.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.main.events({
+  "submit .new-todo": function(event){
+    var text = event.target.text.value;
+    console.log(text);
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date()
+    });
+
+    // clear
+    event.target.text.value = "";
+
+    // prevent submit
+    return false;
+  },
+  "click .toggle-checked": function() {
+    Todos.update(this._id, {$set:{checked: ! this.checked}});
+  },
+  "click .delete-todo": function() {
+    if(confirm('Are you sure?')) {
+      Todos.remove(this._id);
+    }
   }
 });
 
